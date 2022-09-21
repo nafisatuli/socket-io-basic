14f23064834bc53387bc6ef57e75c4152aa61e25
nafisatuli <nafisahasan98@gmail.com>
2022-09-21 12:30:03 +0600

    client to server data transfer.

--- index.js
+++ index.js
@@ -10,28 +10,36 @@ const { Server } = require('socket.io');
 //pass expressServer as a contructor parameter in the object
 const io = new Server(expressServer);
 
+// io.on('connection', function (socket) {
+//     console.log("User Connected");
+
+//     // socket.on('disconnect',function(){
+//     //     console.log("User Disconnected");
+//     // })
+//     // setTimeout(() => {
+//     //     //through this data and catch it on client side
+//     //     socket.send("Server--->Client")
+//     // }, 10000);
+
+//     //continue data transfer
+//     setInterval(() => {
+//         let d = new Date();
+//         let t = d.getTime();
+//         //socket.send(t);
+
+//         //custom event
+//         socket.emit("myEvent", t);
+//     }, 2000);
+// })
+
+
+//client to server data receive
 io.on('connection', function (socket) {
-    console.log("User Connected");
-
-    // socket.on('disconnect',function(){
-    //     console.log("User Disconnected");
-    // })
-    // setTimeout(() => {
-    //     //through this data and catch it on client side
-    //     socket.send("Server--->Client")
-    // }, 10000);
-
-    //continue data transfer
-    setInterval(() => {
-        let d = new Date();
-        let t = d.getTime();
-        //socket.send(t);
-
-        //custom event
-        socket.emit("myEvent", t);
-    }, 2000);
+    console.log("User connected")
+    socket.on('message', function (msg) {
+        console.log(msg)
+    })
 })
-
 
 app.get('/', function (req, res) {
     res.sendFile(__dirname + '/index.html')
