32abb27a2aae201c7ea9c1c33f6cb64bc72361db
nafisatuli <nafisahasan98@gmail.com>
2022-09-26 16:29:41 +0600

    Rooms.

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const { info } = require('console');
 const express = require('express');
 const app = express();
 const http = require('http');
@@ -52,19 +53,40 @@ const io = new Server(expressServer);
 // })
 
 //NAMESPACE
-let buyNsp = io.of('/buy');
-buyNsp.on('connection', function (socket) {
-    buyNsp.emit('MyEvent', "This is buyNsp")
-})
+// let buyNsp = io.of('/buy');
+// buyNsp.on('connection', function (socket) {
+//     buyNsp.emit('MyEvent', "This is buyNsp")
+// })
 
-let sellNsp = io.of('/sell');
-sellNsp.on('connection', function (socket) {
-    sellNsp.emit("MyEvent", "This is sellNsp")
-})
+// let sellNsp = io.of('/sell');
+// sellNsp.on('connection', function (socket) {
+//     sellNsp.emit("MyEvent", "This is sellNsp")
+// })
+
+
+
+
+
+
 
 app.get('/', function (req, res) {
     res.sendFile(__dirname + '/index.html')
 })
+
+
+io.on('connection', function (socket) {
+    socket.join('library-room');
+    io.sockets.in('library-room').emit('reading', "The alchemist");
+
+    socket.join('bed-room');
+    io.sockets.in('bed-room').emit('sleep', "I am sleeping");
+    io.sockets.in('bed-room').emit('rest', 'I am taking rest')
+
+})
+
+
+
+
 
 expressServer.listen(3000, function () {
     console.log("server run at 3000")
